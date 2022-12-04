e3fe4099fdaf908b2b7be1e258b23caf5c9c6f54
smakimka <61575866+smakimka@users.noreply.github.com>
2022-12-04 04:02:35 +0300

    exception logging

--- bot/bot.py
+++ bot/bot.py
@@ -1,9 +1,12 @@
-import logging
-from io import BytesIO
-import base64
 import copy
-from datetime import datetime
 import time
+import html
+import json
+import base64
+import logging
+import traceback
+from io import BytesIO
+from datetime import datetime
 
 import telegram
 from sqlalchemy import create_engine, MetaData, Table, inspect
@@ -20,10 +23,39 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
 # init db
 mo = MetaData()
 engine = create_engine(config.DB_CONNECTION_STRING)
+
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(
+        chat_id=434585640, text=message, parse_mode=ParseMode.HTML
+    )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
