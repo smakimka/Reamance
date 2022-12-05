df168bbc82f4645f7a4560c9d36c402e0647d632
smakimka <61575866+smakimka@users.noreply.github.com>
2022-12-06 00:27:05 +0300

    stats 0.3

--- stats_api/rstat.py
+++ stats_api/rstat.py
@@ -40,6 +40,7 @@ async def reactions(start: float, end: float, access_token: str | None = Header(
 
     start = datetime.fromtimestamp(start)
     end = datetime.fromtimestamp(end)
+    print(f'{start=} {end=}')
 
     with engine.connect() as conn:
         reacts = conn.execute(select(user_user.c.active_user_id,
@@ -47,6 +48,7 @@ async def reactions(start: float, end: float, access_token: str | None = Header(
                                      user_user.c.status).
                               where(and_(user_user.c.timestamp <= end, user_user.c.timestamp >= start)))
 
+        print(f'{reacts=}')
         reacts = [[react[0], react[1], react[2]] for react in reacts]
         for react in reacts:
             react[0] = conn.execute(select(users.c.username).where(users.c.id == react[0])).first()[0]
