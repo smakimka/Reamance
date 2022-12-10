b8cbe8867fb1bb26067bb0a0971db2a7083cd0ad
smakimka <61575866+smakimka@users.noreply.github.com>
2022-12-10 16:50:05 +0300

    user stats 15

--- stats_api/rstat.py
+++ stats_api/rstat.py
@@ -102,16 +102,16 @@ async def reactions(user_login: str, access_token: str | None = Header(default=N
         if response_info['value'] == 'like' or response_info['value'] == 'anonim':
             if user_reacts.get(response) is not None and \
                     (user_reacts[response]['value'] == 'like' or user_reacts[response]['value'] == 'anonim'):
-                matches.append({'user': response, 'timestamp': max(response_info['timestamp'], user_reacts[response]['timestamp'])})
+                matches.append({'user': response, 'timestamp': max(response_info['timestamp'], user_reacts[response]['timestamp'].strftime("%m/%d/%Y, %H:%M:%S"))})
             else:
-                likes.append({'user': response, 'timestamp': response_info['timestamp']})
+                likes.append({'user': response, 'timestamp': response_info['timestamp'].strftime("%m/%d/%Y, %H:%M:%S")})
 
         else:
-            dislikes.append({'user': response, 'timestamp': response_info['timestamp']})
+            dislikes.append({'user': response, 'timestamp': response_info['timestamp'].strftime("%m/%d/%Y, %H:%M:%S")})
 
     timeline = []
     for user_react, react_values in user_reacts.items():
-        timeline.append({'event': f'{user_react} ({react_values["value"]})', 'timestamp': (react_values["timestamp"])})
+        timeline.append({'event': f'{user_react} ({react_values["value"]})', 'timestamp': (react_values["timestamp"].strftime("%m/%d/%Y, %H:%M:%S"))})
 
     return {'likes': {'count': len(likes), 'values': likes},
             'matches': {'count': len(matches), 'values': matches},
