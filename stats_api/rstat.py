f83e2a14f658057224920c6874a4f6de0a59f9a4
smakimka <61575866+smakimka@users.noreply.github.com>
2022-12-10 14:16:30 +0300

    user stats

--- stats_api/rstat.py
+++ stats_api/rstat.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from fastapi import FastAPI, Header, HTTPException
 
-from sqlalchemy import create_engine, Table, MetaData, select, and_
+from sqlalchemy import create_engine, Table, MetaData, select, or_, and_
 from dotenv import load_dotenv
 
 
@@ -63,7 +63,52 @@ async def reactions(start: float, end: float, access_token: str | None = Header(
     return response
 
 
-@app.get("/users/<user_login>")
-async def reactions(start: float, end: float, access_token: str | None = Header(default=None)):
+@app.get("reactions/user/{user_login}/")
+async def reactions(user_login: str, access_token: str | None = Header(default=None)):
     if not access_token or access_token != ACCESS_TOKEN:
-        raise HTTPException(status_code=404, detail="**** you")
+        raise HTTPException(status_code=404, detail='**** you')
+
+    with engine.connect() as conn:
+        user_id = conn.execute(select(users.c.id).where(users.c.username == user_login)).first()
+        if user_id:
+            user_id = user_id[0]
+        else:
+            raise HTTPException(status_code=400, detail='No such user')
+
+        reacts = conn.execute(select(user_user.c.active_user_id,
+                                     user_user.c.passive_user_id,
+                                     user_user.c.status,
+                                     user_user.c.timestamp).
+                              where(or_(user_user.c.active_user_id == user_id,
+                                        user_user.c.passive_user_id == user_id)))
+
+        like_values = ('dislike', 'anonim', 'like')
+
+        user_reacts = {}
+        responses = {}
+        user_login = conn.execute(select(users.c.username).where(users.c.id == user_id)).first()[0]
+        for react in reacts:
+            if react[0] == user_id:
+                user_reacts[user_login] = {'value': like_values[react[2]], 'timestamp': react[3]}
+            else:
+                resp_user_login = conn.execute(select(users.c.username).where(users.c.id == react[0])).first()[0]
+                responses[resp_user_login] = {'value': like_values[react[2]], 'timestamp': react[3]}
+
+    likes = []
+    matches = []
+    dislikes = []
+
+    for response, response_info in responses.items():
+        if response_info['value'] == 'like' or response_info['value'] == 'anonim':
+            if user_reacts.get(response) is not None and \
+                    (user_reacts[response]['value'] == 'like' or user_reacts[response]['value'] == 'anonim'):
+                matches.append(response)
+            else:
+                likes.append(response)
+
+        else:
+            dislikes.append(response)
+
+    return {'likes': {'count': len(likes), 'users': likes},
+            'matches': {'count': len(matches), 'users': matches},
+            'dislikes': {'count': len(dislikes), 'users': dislikes}}
