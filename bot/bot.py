f26f745b02df54a7f7d2265dee43b2465dcbea84
smakimka <61575866+smakimka@users.noreply.github.com>
2022-12-10 13:04:52 +0300

    bug fix (msg too long)

--- bot/bot.py
+++ bot/bot.py
@@ -31,18 +31,13 @@ engine = create_engine(config.DB_CONNECTION_STRING)
 
 
 async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Log the error and send a telegram message to notify the developer."""
-    # Log the error before we do anything else, so we can see it even if something breaks.
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
 
-    # traceback.format_exception returns the usual python message about an exception, but as a
-    # list of strings rather than a single string, so we have to join them together.
     tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
     tb_string = "".join(tb_list)
 
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
+
     message = (
         f"An exception was raised while handling an update\n"
         f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
@@ -52,10 +47,16 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
         f"<pre>{html.escape(tb_string)}</pre>"
     )
 
-    # Finally, send the message
-    await context.bot.send_message(
-        chat_id=434585640, text=message, parse_mode=ParseMode.HTML
-    )
+    if len(message) > 4096:
+        for i in range(0, len(message), 4096):
+            await context.bot.send_message(
+                chat_id=434585640, text=message[i: i + 4096], parse_mode=ParseMode.HTML
+            )
+
+    else:
+        await context.bot.send_message(
+            chat_id=434585640, text=message, parse_mode=ParseMode.HTML
+        )
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
